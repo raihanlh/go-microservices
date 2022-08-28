eeb9192db05e004dee34c31c48f08c494a8c8d49
Raihan Luthfi <raihan.luthfi.h@gmail.com>
2022-08-28 18:11:35 +0700

    feature: add update article api endpoint on gateway microservice

--- gateway-microservice/src/routes/v1/articles.go
+++ gateway-microservice/src/routes/v1/articles.go
@@ -27,7 +27,7 @@ func NewArticleRouter(articleService pb.ArticleServiceClient) routes.Router {
 func (a *ArticleRouter) Route(app *fiber.App) {
 	app.Get("/articles/:id", a.GetArticle)
 	app.Post("/articles", a.CreateArticle)
-	// app.Put("/articles/:id", a.UpdateArticle)
+	app.Patch("/articles/:id", a.UpdateArticle)
 	// app.Delete("/articles", DeleteArticle)
 }
 
@@ -52,7 +52,7 @@ func (a *ArticleRouter) GetArticle(ctx *fiber.Ctx) error {
 	}
 
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
-		"success": true,
+		"message": "success",
 		"data":    res,
 	})
 }
@@ -75,7 +75,7 @@ func (a *ArticleRouter) CreateArticle(ctx *fiber.Ctx) error {
 		Token:   token,
 	})
 	if err != nil {
-		log.Printf("failed create article: %s\n", err.Error())
+		log.Printf("failed to create article: %s\n", err.Error())
 		if e, ok := status.FromError(err); ok {
 			return ctx.Status(util.HTTPStatusFromCode(e.Code())).JSON(&fiber.Map{
 				"success": false,
@@ -90,18 +90,48 @@ func (a *ArticleRouter) CreateArticle(ctx *fiber.Ctx) error {
 	}
 
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
-		"success": true,
+		"message": "success",
 		"data":    res,
 	})
 }
 
-// func (a *ArticleRouter) UpdateArticle(ctx *fiber.Ctx) error {
-// 	var req pb.UpdateArticleRequest
-// 	err := ctx.BodyParser(&req)
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 		return err
-// 	}
-// 	fmt.Println(&req)
-// 	return nil
-// }
+func (a *ArticleRouter) UpdateArticle(ctx *fiber.Ctx) error {
+	authHeader := ctx.Get("Authorization")
+	token := strings.Split(authHeader, "Bearer ")[1]
+	id, _ := strconv.ParseInt(ctx.Params("id"), 10, 64)
+
+	var req pb.CreateArticleRequest
+	err := ctx.BodyParser(&req)
+	if err != nil {
+		return ctx.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"success": false,
+			"error":   err.Error(),
+		})
+	}
+
+	res, err := a.ArticleService.UpdateArticle(context.Background(), &pb.UpdateArticleRequest{
+		Id:      id,
+		Title:   req.Title,
+		Content: req.Content,
+		Token:   token,
+	})
+	if err != nil {
+		log.Printf("failed to update article: %s\n", err.Error())
+		if e, ok := status.FromError(err); ok {
+			return ctx.Status(util.HTTPStatusFromCode(e.Code())).JSON(&fiber.Map{
+				"success": false,
+				"error":   err.Error(),
+			})
+		} else {
+			return ctx.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+				"success": false,
+				"error":   err.Error(),
+			})
+		}
+	}
+
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "success",
+		"data":    res,
+	})
+}
