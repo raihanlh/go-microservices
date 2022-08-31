3dba24cf858a0dc8d257c8deb07b17bf642d0f21
Raihan Luthfi <raihan.luthfi.h@gmail.com>
2022-08-31 11:45:22 +0700

    add create namespace command on configure ingress script

--- configure-ingress.sh
+++ configure-ingress.sh
@@ -2,4 +2,5 @@
 
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
+kubectl create namespace ingress-nginx
 helm install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx -f ingress/ingress-values.yaml
