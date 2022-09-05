b6a060ab99f0641ea21d121cfd21548328ac2139
Raihan Luthfi <raihan.luthfi.h@gmail.com>
2022-09-05 10:35:50 +0700

    add command to apply prometheus' ingress

--- configure-prometheus.sh
+++ configure-prometheus.sh
@@ -1,5 +1,5 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts 
 helm repo update
 helm install prometheus prometheus-community/prometheus -f prometheus/prometheus-values.yaml
-
+kubectl apply -f ingress/ingress-prometheus.yaml
 # helm upgrade prometheus prometheus-community/prometheus -f prometheus/prometheus-values.yaml
