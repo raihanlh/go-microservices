a18a47aad541c9075e57115889cd22c2d2a5d9c9
Raihan Luthfi <raihan.luthfi.h@gmail.com>
2022-09-09 21:56:28 +0700

    create test case for create update user

--- user-microservice/go.mod
+++ user-microservice/go.mod
@@ -3,41 +3,34 @@ module github.com/raihanlh/go-user-microservice
 go 1.17
 
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible
-	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/lib/pq v1.10.6
-	github.com/spf13/viper v1.12.0
-	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
+	github.com/prometheus/client_golang v1.13.0
 	google.golang.org/grpc v1.48.0
 	google.golang.org/protobuf v1.28.1
 )
 
 require (
-	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
-	github.com/gofiber/fiber/v2 v2.36.0 // indirect
-	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.3 // indirect
-	github.com/prometheus/client_golang v1.13.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
 	github.com/prometheus/procfs v0.8.0 // indirect
-	github.com/spf13/afero v1.9.2 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/subosito/gotenv v1.4.0 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.39.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/spf13/viper v1.13.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	github.com/subosito/gotenv v1.4.1 // indirect
 	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	golang.org/x/text v0.3.7 // indirect
---
+++ user-microservice/src/service/user_service_test.go
@@ -0,0 +1,112 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/lib/pq"
+	pb "github.com/raihanlh/go-user-microservice/proto"
+	"github.com/raihanlh/go-user-microservice/src/config"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/test/bufconn"
+	"log"
+	"net"
+	"testing"
+)
+
+const bufSize = 1024 * 1024
+
+var lis *bufconn.Listener
+
+func init() {
+	var db *sql.DB
+
+	configuration, err := config.LoadConfigByPath("../../..")
+	if err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+
+	dbInfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+		"localhost", configuration.DB.User, configuration.DB.Password, configuration.DB.Name)
+
+	db, err = sql.Open("postgres", dbInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	// defer db.Close()
+
+	// Connect to auth service
+	authAddress := fmt.Sprintf("%v:%v", "localhost", configuration.Auth.Port)
+
+	var authConn *grpc.ClientConn
+	authConn, err = grpc.Dial(authAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Could not connect to port %v: %v", configuration.Auth.Port, err)
+	}
+	fmt.Println("Connected to auth gRPC server succesfully")
+
+	// defer authConn.Close()
+
+	authService := pb.NewAuthServiceClient(authConn)
+
+	lis = bufconn.Listen(bufSize)
+	s := grpc.NewServer()
+	userDetailRepository := repository.NewUserDetailRepository(db)
+	pb.RegisterUserDetailServiceServer(s, &ArticleServer{
+		UserDetailRepository: articleRepository,
+		AuthService:          authService,
+	})
+
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Server exited with error: %v", err)
+		}
+	}()
+}
+
+func bufDialer(context.Context, string) (net.Conn, error) {
+	return lis.Dial()
+}
+
+func TestUserDetailService(t *testing.T) {
+	ctx := context.Background()
+	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatalf("Failed to dial bufnet: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewUserDetailServiceClient(conn)
+	auth_token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAbG9jYWwuaG9zdCIsImV4cCI6MTY2MjM1ODU5MCwiaWQiOjl9.Uhmhiv_7V0SUzxlEFxFmQ-Dk_Eh1I8LwlMyk78bqA6U"
+
+	t.Run("Ensure create/update user detail is success", func(t *testing.T) {
+		res, err := client.CreateUpdateUserDetail(ctx, &pb.CreateUpdateUserDetailRequest{
+			Fullname: "Test McTester",
+			IdGender: 0,
+			Phone:    "08123456789",
+			DateOfBirth: &pb.Date{
+				Day:   1,
+				Month: 1,
+				Year:  1997,
+			},
+			Token: auth_token,
+		})
+		if err != nil {
+			t.Log(err)
+			t.Errorf("Create/update user detail test failed")
+		}
+		PrintResult(res)
+	})
+}
+
+func PrintResult(res interface{}) error {
+	result, err := json.MarshalIndent(res, "", " ")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(result))
+	return nil
+}
