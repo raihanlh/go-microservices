a7bf205a0c0f12a20bf177bfe4cd223d989d6f44
Raihan Luthfi <raihan.luthfi.h@gmail.com>
2022-09-10 22:24:40 +0700

    implement Save method on user detail repo

--- user-microservice/src/repository/user_repo_impl.go
+++ user-microservice/src/repository/user_repo_impl.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"time"
 
 	pb "github.com/raihanlh/go-user-microservice/proto"
 )
@@ -18,7 +19,34 @@ func NewUserRepository(db *sql.DB) UserRepository {
 }
 
 func (repo *UserRepositoryImpl) Save(c context.Context, user *pb.UserDetail) (*pb.UserDetail, error) {
-	return nil, nil
+	const query = `INSERT INTO user_details (id_user, fullname, id_gender, phone, date_of_birth, created_at, updated_at, deleted_at) ` +
+		`VALUES (?, ?, ?, ?, ?, ?, ?, ?) RETURNING id, fullname, id_gender, phone, date_of_birth, created_at, updated_at`
+
+	var id int64
+	var fullname string
+	var id_gender string
+	var phone string
+	var dob time.Time
+	var created_at string
+	var updated_at string
+
+	loc := time.FixedZone("UTC+7", 7*60*60)
+	dob = time.Date(int(user.DateOfBirth.Year), time.Month(int(user.DateOfBirth.Month)), int(user.DateOfBirth.Day), 0, 0, 0, 0, loc)
+
+	stmt, err := repo.DB.PrepareContext(c, query)
+	if err != nil {
+		return nil, err
+	}
+	row := stmt.QueryRowContext(c, user.IdAccount, user.Fullname, user.IdGender, user.Phone, dob, user.CreatedAt.AsTime(), user.UpdatedAt.AsTime(), nil)
+
+	err = row.Scan(c, &fullname, &id_gender, &phone, &dob, &created_at, &updated_at)
+	if err != nil {
+		return nil, err
+	}
+
+	user.Id = id
+
+	return user, nil
 }
 
 func (repo *UserRepositoryImpl) Update(c context.Context, user *pb.UserDetail) (*pb.UserDetail, error) {
--- user-microservice/src/repository/user_repo_test.go
+++ user-microservice/src/repository/user_repo_test.go
@@ -9,10 +9,12 @@ import (
 	pb "github.com/raihanlh/go-user-microservice/proto"
 	"github.com/raihanlh/go-user-microservice/src/repository"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
+var current_time = time.Now()
+
 var u = &pb.UserDetail{
-	Id:        1,
 	IdAccount: 1,
 	Fullname:  "Test McTester",
 	IdGender:  0,
@@ -23,6 +25,8 @@ var u = &pb.UserDetail{
 		Month: 1,
 		Day:   1,
 	},
+	CreatedAt: timestamppb.New(current_time),
+	UpdatedAt: timestamppb.New(current_time),
 }
 
 func TestSave(t *testing.T) {
@@ -34,17 +38,15 @@ func TestSave(t *testing.T) {
 	loc := time.FixedZone("UTC+7", 7*60*60)
 
 	dob := time.Date(int(u.DateOfBirth.Year), time.Month(int(u.DateOfBirth.Month)), int(u.DateOfBirth.Day), 0, 0, 0, 0, loc)
-	current_time := time.Now()
 
 	rows := sqlmock.NewRows([]string{"id", "fullname", "id_gender", "phone", "date_of_birth", "created_at", "updated_at"}).
-		AddRow(u.Id, u.Fullname, u.IdGender, u.Phone, dob, current_time, current_time)
+		AddRow(1, u.Fullname, u.IdGender, u.Phone, dob, u.CreatedAt.AsTime(), u.UpdatedAt.AsTime())
 
-	query := "INSERT INTO user_details \\(id, id_user, fullname, id_gender, phone, date_of_birth, created_at, updated_at, deleted_at\\) " +
-		"VALUES \\(\\?, \\?, \\?, \\?, \\?, \\?, \\?, \\?, \\?\\) RETURNING id, fullname, id_gender, phone, date_of_birth, created_at, updated_at"
+	query := "INSERT INTO user_details \\(id_user, fullname, id_gender, phone, date_of_birth, created_at, updated_at, deleted_at\\) " +
+		"VALUES \\(\\?, \\?, \\?, \\?, \\?, \\?, \\?, \\?\\) RETURNING id, fullname, id_gender, phone, date_of_birth, created_at, updated_at"
 
 	prep := mock.ExpectPrepare(query)
-	prep.ExpectQuery().WithArgs(u.Id, u.IdAccount, u.Fullname, u.IdGender, u.Phone, dob, current_time, current_time, nil).WillReturnRows(rows)
-
+	prep.ExpectQuery().WithArgs(u.IdAccount, u.Fullname, u.IdGender, u.Phone, dob, u.CreatedAt.AsTime(), u.UpdatedAt.AsTime(), nil).WillReturnRows(rows)
 	repo := repository.NewUserRepository(db)
 
 	user_detail, err := repo.Save(context.TODO(), u)
