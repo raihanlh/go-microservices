72eda019a0c2e761bf2b622f16f4ddceacc0c971
Raihan Luthfi <raihan.luthfi.h@gmail.com>
2022-10-24 19:17:53 +0700

    Add get user detail method

--- user-microservice/src/service/user_service.go
+++ user-microservice/src/service/user_service.go
@@ -6,6 +6,8 @@ import (
 
 	pb "github.com/raihanlh/go-user-microservice/proto"
 	"github.com/raihanlh/go-user-microservice/src/repository"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type UserServer struct {
@@ -55,4 +57,36 @@ func (server *UserServer) CreateUpdateUserDetail(ctx context.Context, req *pb.Cr
 		return res, nil
 	}
 
+}
+
+func (server *UserServer) GetUserDetailByUser(ctx context.Context, req *pb.GetUserDetailByUserRequest) (*pb.UserDetail, error) {
+	authReq := &pb.GetByTokenRequest{
+		Token: req.Token,
+	}
+	var user *pb.GetUserResponse
+	user, err := server.AuthService.GetByToken(ctx, authReq)
+
+	if err != nil {
+		log.Println("Failed to get token: ", err)
+		return nil, err
+	}
+
+	isExist, err := server.UserDetailRepository.IsExist(ctx, user.Id)
+	log.Println(isExist)
+	if err != nil {
+		log.Println("Failed to get isExist: ", err)
+		return nil, err
+	}
+
+	if !isExist {
+		return &pb.UserDetail{}, status.Error(codes.NotFound, "article not found")
+	} else {
+		log.Println("Getting user detail...")
+		res, err := server.UserDetailRepository.FindByAccountId(ctx, user.Id)
+		if err != nil {
+			log.Println("Failed to get user detail: ", err)
+			return nil, err
+		}
+		return res, nil
+	}
 }
